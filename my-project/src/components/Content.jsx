3f56e531710a084bb85a68b6c33c7a915ce19214
Nilesh <nvrangari@gmail.com>
2024-04-17 20:38:08 +0530

    Navbar Created

---
+++ my-project/src/components/Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+    return(
+        <div>
+            <h >Content</h>
+        </div>
+    )
+}
+
+export default Content
---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+function Navbar() {
+    return(
+        <header className="w-full px-0 py-6 h-[72px] bg-[#191b1fe6] backdrop-blur-sm border-b border-[#191b1f] sticky">
+        <div>
+          <nav>
+            <a className="px-4 py-4 text-[#a3a3a3] hover:text-white cursor-pointer" href="/ecosystem">Ecosystem</a>
+            <a className="px-4 py-4 text-[#a3a3a3] hover:text-white cursor-pointer" href="/community">Community</a>
+            <a className="px-4 py-4 text-[#a3a3a3] hover:text-white cursor-pointer" href="/governance">Governance</a>
+            <a className="px-4 py-4 text-[#a3a3a3] hover:text-white cursor-pointer" href="/developers">Developers</a>
+            <a className="px-4 py-4 text-[#a3a3a3] hover:text-white cursor-pointer" href="https://blog.uniswap.org">Blog</a>
+            <a className="px-4 py-4 text-[#a3a3a3] hover:text-white cursor-pointer" href="/faq">FAQ</a>
+            <a className="px-4 py-4 text-[#a3a3a3] hover:text-white cursor-pointer" href="https://boards.greenhouse.io/uniswaplabs" target="_blank">Jobs</a>
+            <a target="_blank" href="https://app.uniswap.org" className="px-2 py-3 ml-4 rounded-lg cursor-pointer text-white bg-[#ff007a]">Launch App</a>
+          </nav>
+          <div className="">
+            <button className="">Menu</button>
+          </div>
+        </div>
+      </header>
+
+        // <div>
+        //     <h1>Logo</h1>
+        //     <a href="">Ecosystem</a>
+        //     <a href="">Community</a>
+        //     <a href="">Governance</a>
+        //     <a href="">Developers</a>
+        //     <a href="">Blog</a>
+        //     <a href="">FAQ</a>
+        // </div>
+    )
+}
+
+export default Navbar
